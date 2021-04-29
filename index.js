70c6aa16f3e4e4e4c3e961814dbc6caf7ddb1aa2
mrrs878 <mrrs878@foxmail.com>
2021-04-29 10:51:55 +0800

    fix: 修复多行文本渲染bug

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /*
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-04-27 10:47:50
- * @LastEditTime: 2021-04-28 19:47:07
+ * @LastEditTime: 2021-04-29 10:50:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /gif-maker/js/index.js
  */
 const { GIF, alert } = window;
+const CanvasTextBasePosition = { x: 10, y: 30 };
 const Elements = {
   canvas: document.querySelector("#typed"),
   textArea: document.querySelector("#text"),
   fontSizeInput: document.querySelector("#fontSize"),
   confirmBtn: document.querySelector("#confirmBtn"),
+  speedInput: document.querySelector("#speed"),
   renderingModal: document.querySelector("#renderingModal"),
   progressTip: document.querySelector("#progressTip"),
 }
@@ -45,25 +47,29 @@ function initCanvas(canvasElement) {
   })
 }
 
-function drawText({ canvasElement, text, cb, fontSize }) {
-  let strIndex = 0;
+function drawText({ canvasElement, text, cb, fontSize, delay }) {
+  const canvasCtx = canvasElement.getContext("2d");
+  const lines = text.match(/[^\r\n]+/g);
+  let textIndex = 0;
+  let lineTextIndex = 0;
+  let lineIndex = 1;
   return new Promise((resolve) => {
     function draw() {
-      if(strIndex >= text.length) {
+      if(textIndex >= text.length) {
         resolve();
         return;
       }
-      const canvasCtx = canvasElement.getContext("2d");
       canvasCtx.font = `${fontSize || 24}px Sans-serif`;
-      const lines = text.match(/[^\r\n]+/g);
-      for(let i = 0; i < lines.length; i++) {
-        const yL =  30 * (i+1) + 30;
+      const length = text[textIndex].match(/\r?\n/) ? lineIndex++ : lineIndex;
+      lineTextIndex = text[textIndex++].match(/\r?\n/) ? 0 : lineTextIndex;
+      for(let i = 0; i < length; i++) {
+        const yOffset =  30 * (i+1) + CanvasTextBasePosition.y;
         canvasCtx.fillStyle = "#000";
-        canvasCtx.fillText(lines[i].slice(0, strIndex), 10, yL);
+        canvasCtx.fillText(lines[i]?.slice(0, lineTextIndex) || "", CanvasTextBasePosition.x, yOffset);
         canvasCtx.fillStyle = "#f00";
-        canvasCtx.fillText(lines[i].slice(0, ++strIndex), 10, yL);
+        canvasCtx.fillText(lines[i]?.slice(0, ++lineTextIndex) || "", CanvasTextBasePosition.x, yOffset);
       }
-      cb(canvasElement, { copy: true, delay: 200 });
+      cb(canvasElement, { copy: true, delay });
       requestAnimationFrame(draw);
     }
     requestAnimationFrame(draw);
@@ -84,16 +90,20 @@ async function initRender(gif) {
 }
 
 function initEvents(gif, canvasElement, textElement) {
+  const fontSize = Elements.fontSizeInput.value;
+  textElement.setAttribute("style", `font-size: ${fontSize}px`);
   Elements.confirmBtn.addEventListener("click", async () => {
     const text = textElement.value;
+    const fontSize = Elements.fontSizeInput.value;
+    const delay = Elements.speedInput.value;
     if (!text) {
       alert("请输入要生成的文字");
       return;
     }
+    textElement.setAttribute("style", `font-size: ${fontSize}px`);
     Elements.progressTip.innerText = "";
-    const fontSize = Elements.fontSizeInput.value;
     await initCanvas(canvasElement);
-    await drawText({ canvasElement, text, cb: gif.addFrame.bind(gif), fontSize });
+    await drawText({ canvasElement, text, cb: gif.addFrame.bind(gif), fontSize, delay });
     Elements.renderingModal.classList.add("active");
     renderFinished = true;
     gif.render();
@@ -103,7 +113,7 @@ function initEvents(gif, canvasElement, textElement) {
 function init() {
   try {
     const gif = new GIF({
-      workers: 2,
+      workers: 4,
       quality: 1,
       width: 900,
       height: 400,
